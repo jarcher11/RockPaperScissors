0c2588962cb39f16f7f551d015ef5192d4e5fc15
Jason Archer <jasonarcher17@gmail.com>
2021-07-23 12:18:43 -0400

    git commit -m "first commit"

--- RockPaperScissorsGame/Program.cs
+++ RockPaperScissorsGame/Program.cs
@@ -4,9 +4,54 @@ namespace RockPaperScissorsGame
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        static void Main(string[] args) // Class 1
         {
-            Console.WriteLine("Hello World!");
+            string inputPlayer, inputCPU;
+            int randomInt;
+            int scorePlayer = 0;
+            int scoreCPU = 0;
+
+            Console.WriteLine("Pick between ROCK, PAPER, and Scissors:  ");
+            inputPlayer = Console.ReadLine();
+            inputPlayer = inputPlayer.ToUpper();
+
+            Random rnd = new Random();
+
+            randomInt = rnd.Next(1, 4);
+
+            switch (randomInt)
+            {
+                case 1:
+                    inputCPU = "ROCK";
+                    Console.WriteLine("Computer chose ROCK");
+                    if (inputPlayer == "ROCK")
+                    {
+                        Console.WriteLine("DRAW!!\n\n");
+                        // \n\n 
+                    }
+                    else if (inputPlayer == "PAPER")
+                    {
+                        Console.WriteLine("PLAYER WINS!!\n\n");
+                        scorePlayer++;
+                    }
+                    else if (inputPlayer == "SCISSORS")
+                    {
+                        Console.WriteLine("CPU WINS!!/n/n");
+                        scoreCPU++;
+                    }
+                    break;
+                case 2:
+                    inputCPU = "PAPER";
+                    break;
+                case 3:
+                    inputCPU = "SCISSORS";
+                    break;
+                default:
+                    Console.WriteLine("Invalid entry!");
+                    break;
+
+            }
+
         }
     }
 }
