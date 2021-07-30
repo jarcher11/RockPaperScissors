478ba2f555b214ea0b916da843db8d0ce754bb10
Jason Archer <jasonarcher17@gmail.com>
2021-07-29 23:54:12 -0400

    Updated

--- RockPaperScissorsGame/Program.cs
+++ RockPaperScissorsGame/Program.cs
@@ -167,18 +167,4 @@ namespace RockPaperScissorsGame
     } 
 }
 
-/*using System.Diagnostics;
 
-//...
-void StopwatchUsingMethod()
-{
-    //A: Setup and stuff you don't want timed
-    var timer = new Stopwatch();
-    timer.Start();
-
-    //B: Run stuff you want timed
-    timer.Stop();
-
-    TimeSpan timeTaken = timer.Elapsed;
-    string foo = "Time taken: " + timeTaken.ToString(@"m\:ss\.fff");
-}*/
